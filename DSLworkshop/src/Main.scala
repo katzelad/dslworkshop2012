c4665245a7724f67a809dc8e4b3195932f1a029d
Shir Sofer <shirrr@gmail.com>
2012-07-29 00:23:12 +0300

    test "hello"

---
+++ DSLworkshop/src/Main.scala
@@ -0,0 +1,28 @@
+import org.eclipse.swt._
+import org.eclipse.swt.layout._
+import org.eclipse.swt.widgets._
+import org.eclipse.swt.events._
+
+
+object Main {
+
+    def main(args:Array[String]) = {
+    val display = new Display()
+
+    val shell = new Shell(display)
+    shell.setText("MySWT")
+    shell.setSize(200, 100)
+    shell.setLayout(new GridLayout());
+
+    val l1 = new Label(shell, SWT.PUSH);
+    l1.setText("Hello");
+
+    shell.open()
+    while (!shell.isDisposed()) {
+      if (!display.readAndDispatch()) {
+        display.sleep()
+      }
+    }
+    display.dispose()
+  }
+}
